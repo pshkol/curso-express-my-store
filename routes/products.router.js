b9254b2f715984e3922c619f078e54d99c4a0cac
Pavlo Shkolnyy <pshkolnyy@outlook.com>
2021-12-09 18:46:38 +0000

    [11] se agregaron las rutas PATCH /products y DELETE /products

--- routes/products.router.js
+++ routes/products.router.js
@@ -43,4 +43,24 @@ router.post('/', (req, res) => {
   })
 })
 
+router.patch('/:id', (req, res) => {
+  const body = req.body;
+  const { id } = req.params;
+
+  res.json({
+    message: 'created',
+    data: body,
+    id,
+  })
+})
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+
+  res.json({
+    message: "deleted",
+    id,
+  })
+})
+
 module.exports = router;
