6f1cccb36aa2e8087e03f481155e9db0ebdcd071
Pavlo Shkolnyy <pshkolnyy@outlook.com>
2021-12-09 13:13:25 +0000

    se agrego el archivo http-request, se agregaron las rutas /products/:id y /categories/:categoryId/products/:productId

--- index.js
+++ index.js
@@ -11,10 +11,38 @@ app.get('/nueva-ruta', (req, res) => {
 })
 
 app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: "Producto 1",
+      price: 1000,
+    },
+    {
+      name: "Producto 2",
+      price: 2000,
+    }
+  ]
+  );
+})
+
+app.get('/products/:id', (req, res) => {
+  const id = req.params.id;
+
+  res.json(
+    {
+      id: id,
+      name: "Producto 2",
+      price: 2000,
+    }
+  )
+})
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+
   res.json({
-    name: "Producto 1",
-    price: 1000,
-  });
+    categoryId,
+    productId
+  })
 })
 
 app.listen(port, () => {
