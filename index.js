7fb5a89faafda90be5aeb67d77d1070f61892006
Pavlo Shkolnyy <pshkolnyy@outlook.com>
2021-12-09 16:22:43 +0000

    [7] se importo faker y se modifico la ruta /products

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const faker = require('faker');
 const app = express();
 const port = 3000;
 
@@ -11,17 +12,23 @@ app.get('/nueva-ruta', (req, res) => {
 })
 
 app.get('/products', (req, res) => {
-  res.json([
-    {
-      name: "Producto 1",
-      price: 1000,
-    },
-    {
-      name: "Producto 2",
-      price: 2000,
-    }
-  ]
-  );
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+
+  for (let i = 0; i < limit; i++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl()
+    });
+  }
+
+  res.json(products);
+})
+
+app.get('/products/filter', (req, res) => {
+  res.send('Soy un filter');
 })
 
 app.get('/products/:id', (req, res) => {
@@ -34,6 +41,19 @@ app.get('/products/:id', (req, res) => {
       price: 2000,
     }
   )
+})
+
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query;
+
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset,
+    })
+  } else {
+    res.send('No hay parametros');
+  }
 })
 
 app.get('/categories/:categoryId/products/:productId', (req, res) => {
